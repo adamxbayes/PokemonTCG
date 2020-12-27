fa7fe8de504926455799def4cb6d2e4dea1fb738
= <abayes14@gmail.com>
2020-12-27 23:48:09 +0000

    fresh project, installed tcg api made test call

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import {PokemonTCG} from 'pokemon-tcg-sdk-typescript';
 
 @Component({
   selector: 'app-root',
@@ -13,7 +14,7 @@ export class AppComponent {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
   ) {
     this.initializeApp();
   }
@@ -22,6 +23,11 @@ export class AppComponent {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
+      this.test();
     });
+  }
+
+  test(){
+    PokemonTCG.Card.all().then(res =>{ console.log(res, 'res')});
   }
 }
