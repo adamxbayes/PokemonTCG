0200e0b8c0387b048f5d27d058f8a0fa4518f1d1
= <abayes14@gmail.com>
2021-01-01 12:53:00 +0000

    list of cards in each set

--- src/app/card-set-details/card-set-details.page.ts
+++ src/app/card-set-details/card-set-details.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { PokemonTCG } from 'pokemon-tcg-sdk-typescript';
+import { Card } from 'src/models/card.model';
 import { CardSet } from 'src/models/set.model';
 
 @Component({
@@ -17,11 +18,15 @@ export class CardSetDetailsPage implements OnInit {
   public logoUrl: any;
   public name: string;
   releaseDate: string;
+  totalNumberOfCards: number;
+  public cardsInSet: Card[] = [];
 
   constructor(private route: ActivatedRoute, private router: Router) {
     this.route.queryParams.subscribe(params => {
       if (this.router.getCurrentNavigation().extras.state) {
         this.selectedSet = this.router.getCurrentNavigation().extras.state.code;
+      } else {
+        this.router.navigate(['']);
       }
     })
    }
@@ -40,14 +45,29 @@ export class CardSetDetailsPage implements OnInit {
           this.logoUrl = this.setDetails.logoUrl;
           this.name = this.setDetails.name;
           this.releaseDate = this.setDetails.releaseDate;
-          
+          this.totalNumberOfCards = this.setDetails.totalCards;
           console.log(this.setDetails);
+          this.getCardsFromSet();
         });
       this.loading = false;
     } catch {
       this.loading = false;
       console.log('could not grab set');
     };
+  }
+
+  async getCardsFromSet(){
+    try {
+      let params: PokemonTCG.IQuery[] = [{ name: 'set', value: this.setDetails.name }];
+      this.loading = true;
+      await PokemonTCG.Card.where(params).then(cards => {
+        cards.forEach(c => { 
+          this.cardsInSet.push(new Card(c));
+         });
+
+         console.log(this.cardsInSet);
+      })
+    } catch {}
   }
 
 }
